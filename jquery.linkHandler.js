c431cb8b112584c047c39f0e4e13f850eb0296e6
Web Development Labs <webdevlabs@users.noreply.github.com>
2017-03-13 15:51:39 +0200

    Update jquery.linkHandler.js

--- jquery.linkHandler.js
+++ jquery.linkHandler.js
@@ -1,46 +1,6 @@
 /* Dynamic Link Handler
 Author: Simeon Lyubenov (lyubenov@gmail.com)
 Website: www.webdevlabs.com
-
-Attributes:
-data-link = link to resource (use link handler on this element)
-data-ajax = true (send href as ajax request)
-data-confirm = true/msg (if true show default msg, else show data-confirm="msg contents")
-data-toggle = elementID (Disable caller element and show elementID. Usable with data-ajax requests to switch button content)
-
-Default Options:
-onStart: function(){}
-onEnd: function(){}
-AjaxResponseDelimiter: '!'
-
-Examples:
-$('[data-link]').LinkHandler(); (recommended)
-$('a, button').LinkHandler();
-$('.customlink').LinkHandler();
-$('[data-link]').LinkHandler({AjaxResponseDelimiter: ':',
-															onStart: function(){ $('.loading').show(); },
-															onEnd: function(){ }
-														});
-
-<button type="button" data-link="some.php?delete=someid" data-confirm='Are you sure you want to delete this item?'>delete</button>
-<button type='button'
-				data-link='some.php'
-				data-ajax='true'
-				data-confirm='Are you sure?'
-				data-toggle='new_btn_element'
->GO!</button>
-Full example (using all the features):
-<button type="button"
-				data-link="system.php?disable=this"
-				data-confirm="Are you sure you want to disable this?"
-				data-ajax="true"
-				data-toggle="enable_button"
-				id="disable_button">Set off</button>
-<button type="button"
-				data-link="system.php?enable=this"
-				data-ajax="true"
-				data-toggle="disable_button"
-				id="enable_button">Set on</button>
 */
 
 (function( $ ) {
